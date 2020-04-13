9ca3e1a135a1ca72d7ca5a42763f7606f153613b
shichao.wang <shichao.w@outlook.com>
2020-04-14 04:01:42 +0800

    WIP install.sh

---
+++ install.sh
@@ -0,0 +1,10 @@
+
+# install vundle
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+if [ -f "$HOME/.vimrc" ]; then
+    mv "$HOME/.vimrc" "$HOME/.vimrc.bak";
+fi
+
+
+
