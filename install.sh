001820abd9a5ace250f079013b6c5bfda9bd61a3
shichao.wang <shichao.w@outlook.com>
2020-04-21 17:22:33 +0800

    update install.sh to be able detech existed file and backup

--- install.sh
+++ install.sh
@@ -1,15 +1,23 @@
+#!/bin/bash
 
 # install vundle
-if [ -d "~/.vim/bundle/Vundle.vim" ]; then
+if [ ! -d "$HOME/.vim/bundle/Vundle.vim" ]; then
   git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 fi
 
-if [ -f "$HOME/.vimrc" ]; then
-  echo "yes"
-#    mv -i "$HOME/.vimrc" "$HOME/.vimrc.bak";
-fi
-cwd="$(dirname "$(readlink -f $0)")"
+detech_and_backup() {
+  local target_file=$1
+  if [ -e "$target_file" ]; then
+    local backup_file="$target_file.bak"
+    mv -i "$target_file" "$backup_file"
+    echo "backup $target_file to $backup_file"
+  fi
+}
+
+cwd="$(dirname "$(readlink -f "$0")")"
+detech_and_backup "$HOME/.vimrc"
 ln -s "$HOME/.vimrc" "$cwd/vimrc"
+detech_and_backup "$HOME/.vimrc.plugin"
 ln -s "$HOME/.vimrc.plugin" "$cwd/vimrc.plugin"
 
 vim +PluginInstall +qall
