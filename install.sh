012617d5efc4588d0b107f23a1814ec7e0695bea
shichao wang <shichao.w@outlook.com>
2021-08-20 16:53:13 +0800

    Move rcfiles to directories

--- install.sh
+++ install.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-detech_and_backup() {
-  local target_file=$1
-  if [ -e "$target_file" ]; then
-    local backup_file="$target_file.bak"
-    mv -i "$target_file" "$backup_file"
-    echo "backup $target_file to $backup_file"
-  fi
-}
+# detech_and_backup() {
+#  local target_file=$1
+#  if [ -e "$target_file" ]; then
+#    local backup_file="$target_file.bak"
+#    mv -i "$target_file" "$backup_file"
+#    echo "backup $target_file to $backup_file"
+#  fi
+# }
 
-cwd="$(dirname "$(readlink -f "$0")")"
-detech_and_backup "$HOME/.vimrc"
-ln -s "$cwd/vimrc" "$HOME/.vimrc" 
-detech_and_backup "$HOME/.vim-plug.vimrc"
-ln -s "$cwd/vim-plug.vimrc" "$HOME/.vim-plug.vimrc" 
+# cwd="$(dirname "$(readlink -f "$0")")"
+# detech_and_backup "$HOME/.vimrc"
+# ln -s "$cwd/vimrc" "$HOME/.vimrc" 
+# detech_and_backup "$HOME/.vim-plug.vimrc"
+# ln -s "$cwd/vim-plug.vimrc" "$HOME/.vim-plug.vimrc" 
 
