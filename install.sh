e676a6292a549d5d3abc9c66c64ca3ed8e966a9a
Shichao Wang <wangshichao@dbis.nankai.edu.cn>
2021-09-28 10:33:25 +0800

    [Develop] New install script for vim

--- install.sh
+++ install.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-# detech_and_backup() {
-#  local target_file=$1
-#  if [ -e "$target_file" ]; then
-#    local backup_file="$target_file.bak"
-#    mv -i "$target_file" "$backup_file"
-#    echo "backup $target_file to $backup_file"
-#  fi
-# }
+CWD="$(dirname "$(readlink -f "$0")")"
+BACKUP_FOLDER="$CWD/backups"
 
-# cwd="$(dirname "$(readlink -f "$0")")"
-# detech_and_backup "$HOME/.vimrc"
-# ln -s "$cwd/vimrc" "$HOME/.vimrc" 
-# detech_and_backup "$HOME/.vim-plug.vimrc"
-# ln -s "$cwd/vim-plug.vimrc" "$HOME/.vim-plug.vimrc" 
+detech_and_backup() {
+ local target_file=$1
+ if [ -e "$target_file" ]; then
+   local backup_file="$target_file.bak"
+   mv -i "$target_file" "$BACKUP_FOLDER/"
+   echo "backup $target_file to $BACKUP_FOLDER"
+ fi
+}
+
+detech_and_backup "$HOME/.vim"
+ln -s "$CWD/vim" "$HOME/.vim" 
 
