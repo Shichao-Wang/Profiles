f873c31ad7d0e429192a352070b2aa54947d341b
wangshichao <shichao.w@outlook.com>
2022-09-27 18:09:26 +0800

    [feat] install script

--- install.sh
+++ install.sh
@@ -1,17 +1,42 @@
 #!/bin/bash
 
-CWD="$(dirname "$(readlink -f "$0")")"
-BACKUP_FOLDER="$CWD/backups"
+PROFILE_REPO="$(dirname "$(readlink -f "$0")")"
+BACKUP_FOLDER="$PROFILE_REPO/backups"
 
-detech_and_backup() {
- local target_file=$1
- if [ -e "$target_file" ]; then
-   local backup_file="$target_file.bak"
-   mv -i "$target_file" "$BACKUP_FOLDER/"
-   echo "backup $target_file to $BACKUP_FOLDER"
- fi
+function backup_file() {
+  FILE_TO_BACKUP=$1
+  CURRENT_BACKUP_FOLDER="$BACKUP_FOLDER/$(date "+%Y%m%d")"
+  BACKUP_FILE="$CURRENT_BACKUP_FOLDER"_$(basename "$FILE_TO_BACKUP")
+  mv -i "$FILE_TO_BACKUP" "$BACKUP_FILE"
 }
 
-detech_and_backup "$HOME/.vim"
-ln -s "$CWD/vim" "$HOME/.vim" 
+function backup_and_softlink() {
+  NEW_FILE=$1
+  FILE=$2
+  if cmp -s "$FILE" "$NEW_FILE"; then
+    return
+  fi
 
+  if [ -e "$FILE" ]; then
+    backup_file "$FILE"
+  fi
+  mkdir -p "$(dirname "$FILE")"
+  ln -is "$NEW_FILE" "$FILE"
+}
+
+function install_vimrc() {
+  VIMRC="$HOME/.vim/vimrc"
+  INSTALL_VIMRC="$PROFILE_REPO/vim/vimrc"
+
+  backup_and_softlink "$INSTALL_VIMRC" "$VIMRC"
+}
+
+function install_tmux() {
+  TMUX_CONF="$HOME/.config/tmux/tmux.conf"
+  INSTALL_TMUX_CONF="$PROFILE_REPO/tmux/tmux.conf"
+
+  backup_and_softlink "$INSTALL_TMUX_CONF" "$TMUX_CONF"
+}
+
+install_vimrc
+install_tmux
